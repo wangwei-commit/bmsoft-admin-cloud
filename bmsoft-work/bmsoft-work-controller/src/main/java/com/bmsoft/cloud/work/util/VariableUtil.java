a4d5d41c9716f980f20439e2aba54845da6e72f7
wangwei-commit <2318744017@qq.com>
2020-10-28 10:48:34 +0800

    增加yaml内容为list形式的判断

--- bmsoft-work/bmsoft-work-controller/src/main/java/com/bmsoft/cloud/work/util/VariableUtil.java
+++ bmsoft-work/bmsoft-work-controller/src/main/java/com/bmsoft/cloud/work/util/VariableUtil.java
@@ -1,17 +1,16 @@
 package com.bmsoft.cloud.work.util;
 
-import java.io.Serializable;
-import java.util.Map;
-import java.util.function.Function;
-
-import org.yaml.snakeyaml.Yaml;
-
-import com.bmsoft.cloud.base.R;
-import com.bmsoft.cloud.work.enumeration.inventory.VariableType;
-
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
+import com.bmsoft.cloud.base.R;
+import com.bmsoft.cloud.work.enumeration.inventory.VariableType;
 import lombok.extern.slf4j.Slf4j;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 @Slf4j
 public class VariableUtil {
@@ -19,7 +18,7 @@ public class VariableUtil {
 	public static final Integer VARIABLE_ERROR_CODE = 400;
 
 	public static final String VARIABLE_ERROR_MESSAGE = "变量值格式无效";
-	
+
 	public static boolean isJsonValue(String value) {
 		return JSONUtil.isJson(value);
 	}
@@ -37,7 +36,18 @@ public class VariableUtil {
 			return false;
 		}
 	}
-	
+	public static boolean isYamlListValue(String value) {
+		try {
+			if(StrUtil.isBlank(value)) {
+				return false;
+			}
+			Yaml yaml = new Yaml();
+			List<Map<String, Object>> object = yaml.load(value);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
 	public static boolean vaildVariable(VariableType type, String value) {
 		log.info("VariableType = {}, VariableValue = {}", type, value);
 		if ((VariableType.YAML.eq(type) && !VariableUtil.isYamlValue(value))
@@ -46,7 +56,7 @@ public class VariableUtil {
 		}
 		return true;
 	}
-	
+
 	@SuppressWarnings("rawtypes")
 	public static R handler(VariableType type, String value, Serializable dto, Function<Serializable, R> function) {
 		if (!vaildVariable(type, value)) {
@@ -54,5 +64,5 @@ public class VariableUtil {
 		}
 		return function.apply(dto);
 	}
-	
+
 }
