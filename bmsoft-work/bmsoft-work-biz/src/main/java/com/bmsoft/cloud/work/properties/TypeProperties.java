d1467d89f079973a2e74a98bcde4d2eb90054de8
wangwei-commit <2318744017@qq.com>
2020-10-28 10:26:59 +0800

    增加 同步地址配置

--- bmsoft-work/bmsoft-work-biz/src/main/java/com/bmsoft/cloud/work/properties/TypeProperties.java
+++ bmsoft-work/bmsoft-work-biz/src/main/java/com/bmsoft/cloud/work/properties/TypeProperties.java
@@ -1,11 +1,10 @@
 package com.bmsoft.cloud.work.properties;
 
-import java.util.List;
-
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-import lombok.Data;
+import java.util.List;
 
 @Data
 @RefreshScope
@@ -13,49 +12,62 @@ import lombok.Data;
 public class TypeProperties {
 
 	private List<Type> inventorySource;
-	
+
 	private List<Type> certificateType;
-	
+
+	private List<PlateForm> platformSource;
+	//剧本导入地址
+	private String fileUrl;
+	//作业模板同步地址
+	private String templateUrl;
 	@Data
 	public static class Type {
-		
+
 		private String key;
-		
+
 		private String display;
-		
+
 		private List<TypeField> fields;
 	}
-	
+	@Data
+	public static class PlateForm {
+
+		private String id;
+
+		private String label;
+
+		private List<PlateForm> children;
+	}
 	@Data
 	public static class TypeField {
-		
+
 		private String display;
-		
+
 		private String field;
-		
+
 		private FieldType fieldType;
-		
+
 		private Boolean primary;
-		
+
 		private Integer len;
-		
+
 		private List<SelectData> data;
-		
+
 		private Boolean isDateInterval;
-		
+
 		private Boolean isMulti;
-		
+
 		private Boolean isPWRun;
 	}
-	
+
 	@Data
 	public static class SelectData {
-		
+
 		private String key;
-		
+
 		private String value;
 	}
-	
+
 	public static enum FieldType {
 		TEXT, PASSWORD, MULTI_TEXT, SELECT, CHECKBOX, REDIO, DATE, DATETIME, CERTIFICATE, INVENTORY_SCRIPT, VARIABLE
 	}
